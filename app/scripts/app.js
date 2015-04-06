b55b2d0de92a7dbaf307046076a4fa7829b463b6
Garrett Kucinski <garrett.kucinski@gmail.com>
2015-04-05 22:42:01 -0500

    wiring up angular, can't get routes working

---
+++ app/scripts/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name garrettApp
+ * @description
+ * # garrettApp
+ *
+ * Main module of the application.
+ */
+
+angular
+  .module('compLibApp', [
+    'ngRoute',
+    'ngResource',
+  ])
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainController'
+      })
+      .when('/home', {
+        templateUrl: 'views/home.html',
+        controller: 'HomeController'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutController'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+
+      // Clean up URL's to get rid of # and keep them readable
+
+      $locationProvider.html5Mode(true);
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,4 @@
+app.module('compLibApp')
+  .controller('MainController', function($scope) {
+    $scope.greeting = "Hello, World";
+  });
