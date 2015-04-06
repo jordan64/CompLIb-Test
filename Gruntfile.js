6ab9f575cd2d17cb5295729cbf7cc04a57781001
Garrett Kucinski <garrett.kucinski@gmail.com>
2015-04-05 22:09:40 -0500

    no more errors

--- Gruntfile.js
+++ Gruntfile.js
@@ -198,7 +198,7 @@ module.exports = function (grunt) {
           }
       },
       sass: {
-        src: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
+        src: ['<%= yeoman.app %>/scss/{,*/}*.{scss,sass}'],
         ignorePath: /(\.\.\/){1,2}bower_components\//
       }
     },
