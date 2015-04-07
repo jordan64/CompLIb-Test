bdded6b1668c1a0c10a4c430c6739b6b8e015815
Garrett Kucinski <garrett.kucinski@gmail.com>
2015-04-07 08:54:45 -0500

    added id to each item, added details page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,46 +2,55 @@ angular.module('compLibApp')
   .controller('MainController', function($scope) {
     $scope.vignettes = [
       {
+        id: '1',
         name: 'Finance',
         image: '//placehold.it/740x350/cccccc/FFFFFF.png',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'
       },
       {
+        id: '2',
         name: 'Finance 2',
         image: '//placehold.it/740x350/cccccc/FFFFFF.png',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'
       },
       {
+        id: '3',
         name: 'Finance 3',
         image: '//placehold.it/740x350/cccccc/FFFFFF.png',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'
       },
       {
+        id: '4',
         name: 'Economics',
         image: '//placehold.it/740x350/cccccc/FFFFFF.png',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'
       },
       {
+        id: '5',
         name: 'Business Leadership',
         image: '//placehold.it/740x350/cccccc/FFFFFF.png',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'
       },
       {
+        id: '6',
         name: 'Business Development',
         image: '//placehold.it/740x350/cccccc/FFFFFF.png',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'
       },
       {
+        id: '7',
         name: 'Business Ethics',
         image: '//placehold.it/740x350/cccccc/FFFFFF.png',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'
       },
       {
+        id: '8',
         name: 'Business Ethics 2',
         image: '//placehold.it/740x350/cccccc/FFFFFF.png',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'
       },
       {
+        id: '9',
         name: 'Business Ethics 3',
         image: '//placehold.it/740x350/cccccc/FFFFFF.png',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'
